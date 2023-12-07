662991796d0b9091b85a80977140b04527b840ac
Aditya Kohli <96744303+Adikohli@users.noreply.github.com>
2023-12-07 13:50:03 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1479;
+let accountEmail = thisisaditya@gmail.com;
+var accountPassword = "12345";
+accountCity = "Pithoragarh";
+
+//accountId = 2;   // not allowed beacuse we cant change constant value.
+
+console.log(accountId);
